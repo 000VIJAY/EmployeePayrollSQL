d92e80337ec0d582a66d0d100b299454ba546fd8
000VIJAY <vijay893689@gmail.com>
2022-07-22 15:41:19 +0530

    [Vijay] Added Extended data Employee Pay and tax in database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -44,11 +44,13 @@ SELECT MIN (Salary) FROM Employee_Payroll Where Gender = 'F';
 SELECT MAX (Salary) FROM Employee_Payroll Where Gender = 'F';  
 SELECT COUNT (Salary) FROM Employee_Payroll Where Gender ='F';
 
-
 ALTER table Employee_Payroll ADD PhoneNumber float , Address varchar(300) ; 
 ALTER table Employee_Payroll ADD Department varchar(80) NOT NULL Default 'Dept engg' 
 ALTER table Employee_Payroll drop column Department;
 ALTER TABLE Employee_Payroll ALTER COLUMN PhoneNumber BigInt
 UPDATE Employee_Payroll Set PhoneNumber = 8847283534 , Address = '1-2-3Bihar' , Department = 'Engg' where Id = 1;
 UPDATE Employee_Payroll Set PhoneNumber = 8847283536 , Address = '1-2-3patna' , Department = 'resourse' where Id = 2;
+
+Alter table Employee_Payroll Add BasicPay BIgInt , Deductions BigInt , TaxablePay BigInt ,IncomeTax BigInt ,NetPay BigInt;
+UPDATE Employee_Payroll Set BasicPay = 10000 , Deductions = 500 , TaxablePay = 2000 , IncomeTax = 1000 , NetPay = 10500 where name = 'Md Ayub';
 SELECT * FROM Employee_Payroll; 
