1c6d3921b0a5c287395ec89558f8cd3ec113e23e
000VIJAY <vijay893689@gmail.com>
2022-07-22 13:21:09 +0530

    [Vijay] Added Create a table in database using sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,9 @@
 create database Employee_Payroll
 USE Employee_Payroll;
+create table Employee_Payroll
+(
+	Id		INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	name	VARCHAR (150) NOT NULL,
+	Salary	float NOT NULL ,
+	Start	Date NOT NULL
+);
