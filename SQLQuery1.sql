890c4c5baa3eb13f2003efc0ebce85a05b5bc12c
000VIJAY <vijay893689@gmail.com>
2022-07-22 13:31:35 +0530

    [Vijay] Added Insert data in database using sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -7,3 +7,9 @@ create table Employee_Payroll
 	Salary	float NOT NULL ,
 	Start	Date NOT NULL
 );
+Insert into Employee_Payroll
+(
+	[name] , [Salary] , [start] )Values 
+('Vijay Kumar ', 100000 ,'12-10-2021'),
+('Nitish Kumar' , 50000 , '03-01-2014'),
+('Md Ayub' , 200000 , '06-04-2019')
