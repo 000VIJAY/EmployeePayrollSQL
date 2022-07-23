944bea12ee9a5f0b38ab760ff5a8b968c52bce49
000VIJAY <vijay893689@gmail.com>
2022-07-23 11:35:27 +0530

    [Vijay] Ability to Provide same person in two different Department

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -52,5 +52,14 @@ UPDATE Employee_Payroll Set PhoneNumber = 8847283534 , Address = '1-2-3Bihar' , 
 UPDATE Employee_Payroll Set PhoneNumber = 8847283536 , Address = '1-2-3patna' , Department = 'resourse' where Id = 2;
 
 Alter table Employee_Payroll Add BasicPay BIgInt , Deductions BigInt , TaxablePay BigInt ,IncomeTax BigInt ,NetPay BigInt;
-UPDATE Employee_Payroll Set BasicPay = 10000 , Deductions = 500 , TaxablePay = 2000 , IncomeTax = 1000 , NetPay = 10500 where name = 'Md Ayub';
+UPDATE Employee_Payroll Set BasicPay = 10000 , Deductions = 500 , TaxablePay = 9500 , IncomeTax = 500 , NetPay = 9000 where name = 'Md Ayub';
 SELECT * FROM Employee_Payroll; 
+Insert into Employee_Payroll
+(
+	[name] , [Salary] , [start] )Values 
+('Terissa', 150000 ,'12-10-2020');
+UPDATE Employee_Payroll Set Gender = 'F' ,PhoneNumber = 5783465853 , Address = '4-5-6 Ambala' , Department = 'Sales' , BasicPay = 25000 , Deductions = 1250 , TaxablePay = 23750 , IncomeTax = 2000 , NetPay = 21750 where Id = 6;
+Insert into Employee_Payroll
+(
+	[name] ,[Salary] ,[start] ,[Department] ,[Gender] )Values 
+('Terissa', 0 ,'12-10-2020' ,'Marketing' , 'F');
